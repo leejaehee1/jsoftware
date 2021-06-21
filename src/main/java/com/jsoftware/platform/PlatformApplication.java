76289c79ed58d408dfb797f2ead61cec584cc51d
John Lee <q7phavoc2@gmail.com>
2021-06-22 08:17:45 +0900

    admin redis

--- src/main/java/com/jsoftware/platform/PlatformApplication.java
+++ src/main/java/com/jsoftware/platform/PlatformApplication.java
@@ -1,5 +1,6 @@
 package com.jsoftware.platform;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @MapperScan(basePackageClasses = PlatformApplication.class)
 @SpringBootApplication
 @EnableRedisHttpSession
+@EnableAdminServer
 public class PlatformApplication {
 
 	public static void main(String[] args) {
